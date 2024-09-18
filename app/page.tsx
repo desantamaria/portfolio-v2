a14eabb10b1dda56a5f29f01edaa77f643c8238d
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-09-18 15:36:06 -0700

    Complete BentoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
@@ -10,11 +10,12 @@ export default function Home() {
     justify-center items-center flex-col overflow-hidden 
     mx-auto sm:px-10 px-5"
     >
-      <div className="max-w-7xl w-full h-screen">
+      <div className="max-w-7xl w-full ">
         <FloatingNav
           navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
